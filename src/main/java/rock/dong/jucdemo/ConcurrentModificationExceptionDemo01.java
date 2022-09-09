dccbee01d2bc24c83153e5519e2db34a632dff6a
Rock <1982-615@163.com>
2022-09-09 21:14:11 +0800

    comments update

--- src/main/java/rock/dong/jucdemo/ConcurrentModificationExceptionDemo01.java
+++ src/main/java/rock/dong/jucdemo/ConcurrentModificationExceptionDemo01.java
@@ -5,7 +5,6 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * java.util.ConcurrentModificationException demo, illustrate the Collection is not multi-thread safe.
- *
  */
 public class ConcurrentModificationExceptionDemo01 {
 
@@ -17,12 +16,12 @@ public class ConcurrentModificationExceptionDemo01 {
         //ArrayList arrayList = new ArrayList();
         //copyonwrite is an optimized strategy
         //set is the same as list, but map is different, we might use concurrentHashMap
-        List <Object> objects = new CopyOnWriteArrayList<>();
-;        for (int i = 0; i < 10 ; i++) {
-            new Thread(()->{
-                objects.add(UUID.randomUUID().toString().substring(0,5));
+        List<Object> objects = new CopyOnWriteArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                objects.add(UUID.randomUUID().toString().substring(0, 5));
                 System.out.println(objects);
-            },String.valueOf(i)).start();
+            }, String.valueOf(i)).start();
         }
 
     }
