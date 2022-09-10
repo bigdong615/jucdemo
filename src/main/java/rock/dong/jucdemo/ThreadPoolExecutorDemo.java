fbb86f83e0c9a6af24834f7a2c3a47cb673174e4
Rock <1982-615@163.com>
2022-09-10 12:15:15 +0800

    comments update

---
+++ src/main/java/rock/dong/jucdemo/ThreadPoolExecutorDemo.java
@@ -0,0 +1,31 @@
+package rock.dong.jucdemo;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolExecutorDemo {
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadpool = new ThreadPoolExecutor(2,
+                5,
+                3,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(3),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());
+
+        try { // max_load = dque+ maximumPoolSize 3+5
+            for (int i = 1; i <= 8; i++) {
+                threadpool.execute(() -> {
+                    System.out.println(Thread.currentThread().getName());
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            threadpool.shutdown();
+        }
+    }
+}
